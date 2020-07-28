cf05011507311da8f434e02110292a66b55d1a06
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-28 10:50:57 +0100

    Update ng-slideshow.js

--- ng-slideshow.js
+++ ng-slideshow.js
@@ -4,7 +4,7 @@
 	
 	let style = document.createElement("style"); document.head.appendChild(style);
 	style.textContent = [
-		".ng-slideshow img { width:100%; height:100%; object-fit:cover; position:absolute; top:0; left:0; opacity:0; transform:scale(1.1); z-index:0; }",
+		".ng-slideshow img { width:100%; height:100%; object-fit:cover; position:absolute; top:0; left:0; opacity:0; transform:scale(1.1); z-index:0; display:none; }",
 		".ng-slideshow a:nth-of-type(odd) img { transform-origin:20% 40%; } .ng-slideshow a:nth-of-type(even) img { transform-origin:80% 40%; }",
 		"@keyframes ng-slideshow-fadeIn { 0% { opacity:0; transform:scale(1.0); z-index:1; } 50%, 100% { opacity:1; transform:scale(1.1); z-index:1; } }",
 		"@keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }"
@@ -19,6 +19,7 @@
 		slideshow.style.border = "3px solid lightgrey"; 
 
 		slideshow.querySelectorAll("img").forEach(function ($v, $i, $a) {
+			$v.style.display = "block";
 			$v.onanimationend = function () {
 				$v.style.animation = "ng-slideshow-fadeOut 3s";
 				$a[(($i === $a.length - 1) ? 0 : $i+1)].style.animation = "ng-slideshow-fadeIn 10s";
