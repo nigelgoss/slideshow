e4fdf32d0616d65ccce2a997c53249a0debdafac
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-28 10:44:43 +0100

    Update ng-slideshow.js

--- ng-slideshow.js
+++ ng-slideshow.js
@@ -10,20 +10,23 @@
 		"@keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }"
 	].join("\n");
 	
-	let slideshow = document.querySelector(".ng-slideshow");
-	slideshow.style.display = "inline-block";
-	slideshow.style.position = "relative";
-	slideshow.style.overflow = "hidden";
-	slideshow.style.margin = "3px";
-	slideshow.style.border = "3px solid lightgrey"; 
+	document.querySelectorAll(".ng-slideshow").forEach(function (slideshow) {
 	
-	slideshow.querySelectorAll("img").forEach(function ($v, $i, $a) {
-		$v.onanimationend = function () {
-			$v.style.animation = "ng-slideshow-fadeOut 3s";
-			$a[(($i === $a.length - 1) ? 0 : $i+1)].style.animation = "ng-slideshow-fadeIn 10s";
-		}; 
+		slideshow.style.display = "inline-block";
+		slideshow.style.position = "relative";
+		slideshow.style.overflow = "hidden";
+		slideshow.style.margin = "3px";
+		slideshow.style.border = "3px solid lightgrey"; 
+
+		slideshow.querySelectorAll("img").forEach(function ($v, $i, $a) {
+			$v.onanimationend = function () {
+				$v.style.animation = "ng-slideshow-fadeOut 3s";
+				$a[(($i === $a.length - 1) ? 0 : $i+1)].style.animation = "ng-slideshow-fadeIn 10s";
+			}; 
+		});
+
+		slideshow.querySelector("img").style.animation = "ng-slideshow-fadeIn 10s";
+	
 	});
-	
-	slideshow.querySelector("img").style.animation = "ng-slideshow-fadeIn 10s";
 	
 })();
