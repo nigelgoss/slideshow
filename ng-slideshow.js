bb65d65cd4e858ab68df3a5d4782cb9334fb968a
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-28 10:38:57 +0100

    Update ng-slideshow.js

--- ng-slideshow.js
+++ ng-slideshow.js
@@ -7,7 +7,8 @@
 		".ng-slideshow img { width:100%; height:100%; object-fit:cover; position:absolute; top:0; left:0; opacity:0; transform:scale(1.1); z-index:0; }",
 		".ng-slideshow a:nth-of-type(odd) img { transform-origin:20% 40%; } .ng-slideshow a:nth-of-type(even) img { transform-origin:80% 40%; }",
 		"@keyframes ng-slideshow-fadeIn { 0% { opacity:0; transform:scale(1.0); z-index:1; } 50%, 100% { opacity:1; transform:scale(1.1); z-index:1; } }",
-		"@keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }";
+		"@keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }"
+	].join("\n");
 	
 	let slideshow = document.querySelector(".ng-slideshow");
 	slideshow.style.display = "inline-block";
