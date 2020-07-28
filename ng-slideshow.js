3586574e2118548a83253afd698ee568aedb9f07
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-28 10:36:39 +0100

    Update ng-slideshow.js

--- ng-slideshow.js
+++ ng-slideshow.js
@@ -2,15 +2,19 @@
 	
 	if (!NodeList.prototype.forEach) NodeList.prototype.forEach = Array.prototype.forEach;
 	
+	let style = document.createElement("style"); document.head.appendChild(style);
+	style.textContent = [
+		".ng-slideshow img { width:100%; height:100%; object-fit:cover; position:absolute; top:0; left:0; opacity:0; transform:scale(1.1); z-index:0; }",
+		".ng-slideshow a:nth-of-type(odd) img { transform-origin:20% 40%; } .ng-slideshow a:nth-of-type(even) img { transform-origin:80% 40%; }",
+		"@keyframes ng-slideshow-fadeIn { 0% { opacity:0; transform:scale(1.0); z-index:1; } 50%, 100% { opacity:1; transform:scale(1.1); z-index:1; } }",
+		"@keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }";
+	
 	let slideshow = document.querySelector(".ng-slideshow");
 	slideshow.style.display = "inline-block";
 	slideshow.style.position = "relative";
 	slideshow.style.overflow = "hidden";
 	slideshow.style.margin = "3px";
 	slideshow.style.border = "3px solid lightgrey"; 
-	
-	let style = document.createElement("style"); slideshow.appendChild(style);
-	style.textContent = ".ng-slideshow img { width:100%; height:100%; object-fit:cover; position:absolute; top:0; left:0; opacity:0; transform:scale(1.1); z-index:0; } .ng-slideshow a:nth-of-type(odd) img { transform-origin:20% 40%; } .ng-slideshow a:nth-of-type(even) img { transform-origin:80% 40%; } @keyframes ng-slideshow-fadeIn { 0% { opacity:0; transform:scale(1.0); z-index:1; } 50%, 100% { opacity:1; transform:scale(1.1); z-index:1; } } @keyframes ng-slideshow-fadeOut { 0% { opacity:1; } 100% { opacity:0.5; } }";
 	
 	slideshow.querySelectorAll("img").forEach(function ($v, $i, $a) {
 		$v.onanimationend = function () {
